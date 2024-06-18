a977965839e48226108527cc7f296e18fd190a68
u1988481 <160512411+u1988481@users.noreply.github.com>
2024-06-19 01:40:53 +0200

    Delete JocWeb_equip directory

--- JocWeb_equip/js/menu.js
+++
@@ -1,6 +0,0 @@
-addEventListener('load', function() {
-    document.getElementById('boton_menu').addEventListener('click', 
-    function(){
-        window.location.assign("./html/game.html");
-    });
-});
